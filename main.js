cbc7b45bc00676d73813e4072a30cf5e0ec7a8a2
Aman <aman.mender@tegningtil3d.no>
2019-01-18 19:29:36 +0100

    Basic electron startpoint

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const {app,BrowserWindow} = require('electron')
 // Lifecycle of the app is managed by electron.app
 
 // Windows created by electron.BrowserWindow
+let win
 
 function createWindow() {
     // Create the browser window
@@ -11,6 +12,28 @@ function createWindow() {
     // Load the index html
     win.loadFile('index.html')
 
+    // Open devtools
+    win.webContents.openDevTools();
+
+    // Close window event
+    win.on('close', () => {
+        win = null;
+    })
 }
 
 app.on('ready',createWindow);
+
+// Quit when window closes
+app.on('window-all-closed', () => {
+    // MacOS exemption
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  
+  // Reactivate app when icon is clicked
+  app.on('activate', () => {
+    if (win === null) {
+      createWindow()
+    }
+  })
